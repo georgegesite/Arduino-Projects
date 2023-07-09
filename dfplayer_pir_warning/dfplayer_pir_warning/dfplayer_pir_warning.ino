5cc1e571ddc2f7e7645bfd6091fbbb69a179bf79
George Gesite <104239892+georgegesite@users.noreply.github.com>
2023-07-09 11:23:44 +0800

    PIR sensor Testing
    Testing sensitivity of of pir Sensor

--- dfplayer_pir_warning/dfplayer_pir_warning/dfplayer_pir_warning.ino
+++ dfplayer_pir_warning/dfplayer_pir_warning/dfplayer_pir_warning.ino
@@ -6,28 +6,35 @@ DFRobotDFPlayerMini myDFPlayer;
 void printDetail(uint8_t type, int value);
 
 #define SENSORPIN 9
-#define PAUSETIME 20000
+#define PAUSETIME 10000
+#define RED_LED 4
+#define GREEN_LED 5
 
 void setup() {
   mySoftwareSerial.begin(9600);
   Serial.begin(19200);
   pinMode(SENSORPIN, INPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
 
   Serial.println();
   Serial.println(F("Initializing DFPlayer..."));
-
+  delay(1000);
   //Use softwareSerial to communicate with MP3
   if (!myDFPlayer.begin(mySoftwareSerial)) {
+    digitalWrite(RED_LED, HIGH);   
     Serial.println(F("Unable to begin:"));
     Serial.println(F("1.Please recheck the connection!"));
     Serial.println(F("2.Please insert the SD card!"));
+
     while (true)
       ;
   }
   Serial.println(F("DFPlayer Mini online."));
+  digitalWrite(GREEN_LED, HIGH);  
 
   //Set volume value (From 0 to 30)
-  myDFPlayer.volume(25);
+  myDFPlayer.volume(30);
 }
 
 void sensorActivated() {
